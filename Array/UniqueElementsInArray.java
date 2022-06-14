a822950b98056caa7c6d9202748c94722298fe38
ashDeqode <asiwal@deqode.com>
2022-06-14 09:49:53 +0530

    Array program to find unique elements in array

---
+++ Array/UniqueElementsInArray.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class UniqueElementsInArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size of array : ");
+		
+		int size = sc.nextInt();
+		
+		System.out.println("Enter elements of Array  :");
+		
+		int[] arr = new int[size];
+		TreeSet<Integer> ts = new TreeSet<>();
+		
+		for(int i = 0 ; i < size; i++) {
+			arr[i] = sc.nextInt();
+			ts.add(arr[i]);
+		}
+		
+		System.out.println(ts.size());
+		sc.close();
+	}
+
+}
